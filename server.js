bd855e0c2605acc04e84ef75fc50182daac6147a
Jin Hyuk <wlsgurrla10@naver.com>
2021-09-13 21:47:00 +0900

    [Backend set up] test to apollo/graphQL server

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 import {ApolloServer, gql} from "apollo-server";
 
 const typeDefs = gql`
+    type Movie {
+        title: String, 
+        year: Int
+    }
     type Query{
-        hello: String
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation {
+        createMovie(title: String!): Boolean,
+        deleteMovie(title: String!): Boolean,
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "hiiiii"
+        movies: () => [],
+        movie: () => ({
+            title: "hello",
+            year: 2021
+        })
+    },
+    Mutation: {
+        createMovie: (_, {title}) => {
+            console.log(title);
+            return true;
+        },
+        deleteMovie: (_, {title}) => {
+            console.log(title);
+            return true;
+        }
     }
 }
 
